e352fd5b1b0f00da67b84c58f9d456091c92c2f3
unknown <nononrayhan@gmail.com>
2021-03-02 16:00:02 +0700

    check for init proj dir.

---
+++ Assets/ROTATE_TOUCH.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ROTATE_TOUCH : MonoBehaviour {
+[SerializeField] float rotationSpeed = 100f;
+bool dragging = false;
+Rigidbody rb;  
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent <Rigidbody> ();
+    }
+
+    void OnMouseDrag() {
+        dragging = true;
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            dragging = false;
+        }
+    }
+    
+    void FixedUpdate() {
+        if (dragging) {
+            float x = Input.GetAxis ("Mouse X")*rotationSpeed*Time.fixedDeltaTime;
+            float y = Input.GetAxis ("Mouse Y")*rotationSpeed*Time.fixedDeltaTime;
+
+            rb.AddRelativeTorque (Vector3.down*x);
+            rb.AddRelativeTorque (Vector3.right*y);
+        }   
+    }        
+}
